61466f724937266502b1e9bf9135fda3f05ba90a
Sid Saha <admin@Shameeks-MacBook-Pro-Trimian.local>
2017-05-08 20:08:34 +0530

    matching completed

--- src/match.js
+++ src/match.js
@@ -64,6 +64,111 @@ const multipleRelationshipTypes = () => {
   })
 }
 
+const addSpaceRelationship = () => {
+  const q = `
+    MATCH (rob :Person { name: 'Rob Reiner' }), (charlie :Person { name: 'Charlie Sheen' })
+    CREATE (rob)-[r :\`TYPE WITH SPACE\`]->(charlie)
+    RETURN r
+  `
+  console.log(chalk.dim.magenta('[Q]'), chalk.dim(q))
+  return GraphUtils.runQueryAndFetchResult(q)
+  .then(result => {
+    console.log(chalk.dim.magenta('[Result]'), chalk.dim(JSON.stringify(result)))
+    return Promise.resolve()
+  })
+}
+
+const multipleRelationships = () => {
+  // Find the movie in which Charlie Sheen acted and the director of those movies
+  const q = `
+    MATCH (charlie :Person { name: 'Charlie Sheen' })-[:ACTED_IN]->(movie :Movie)<-[:DIRECTED]-(director :Person)
+    RETURN movie, director
+  `
+  console.log(chalk.dim.magenta('[Q]'), chalk.dim(q))
+  return GraphUtils.runQueryAndFetchResult(q)
+  .then(result => {
+    console.log(chalk.dim.magenta('[Result]'), chalk.dim(JSON.stringify(result)))
+    _.forEach(result, row => {
+      const movieName = row.movie.properties.name
+      const directorName = row.director.properties.name
+      console.log(chalk.magenta('[Movie]'), chalk.bold(movieName),
+          chalk.magenta('[Director]'), chalk.bold(directorName))
+    })
+    return Promise.resolve()
+  })
+}
+
+const variableLengthRelationship = function() {
+  const q = `
+    MATCH (martin :Person { name: 'Martin Sheen' })-[:ACTED_IN*1..3]->(movie :Movie)
+    RETURN movie
+  `
+  console.log(chalk.dim.magenta('[Q]'), chalk.dim(q))
+  return GraphUtils.runQueryAndFetchResult(q)
+  .then(result => {
+    console.log(chalk.dim.magenta('[Result]'), chalk.dim(JSON.stringify(result)))
+    _.forEach(result, row => {
+      const movieName = row.movie.properties.name
+      console.log(chalk.magenta('[Movie]'), chalk.bold(movieName))
+    })
+    return Promise.resolve()
+  })
+}
+
+const coActors = function() {
+  // Find Martin Sheen's coactors in movies
+  const q = `
+    MATCH (martin :Person { name: 'Martin Sheen' })-[:ACTED_IN]->(movie :Movie)<-[:ACTED_IN]-(actor :Person)
+    RETURN actor, movie
+  `
+  console.log(chalk.dim.magenta('[Q]'), chalk.dim(q))
+  return GraphUtils.runQueryAndFetchResult(q)
+  .then(result => {
+    console.log(chalk.dim.magenta('[Result]'), chalk.dim(JSON.stringify(result)))
+    _.forEach(result, row => {
+      const movieName = row.movie.properties.name
+      const actorName = row.actor.properties.name
+      console.log(chalk.magenta('[Movie]'), chalk.bold(movieName),
+          chalk.magenta('[Actor]'), chalk.bold(actorName)) 
+    })
+    return Promise.resolve()
+  })
+}
+
+const namedPath = function() {
+  const q = `
+    MATCH p = (mike :Person { name: 'Michael Douglas' })-->()
+    RETURN p
+  `
+  console.log(chalk.dim.magenta('[Q]'), chalk.dim(q))
+  return GraphUtils.runQueryAndFetchResult(q)
+  .then(result => {
+    console.log(chalk.dim.magenta('[Result]'), chalk.dim(JSON.stringify(result)))
+    return Promise.resolve()
+  })
+}
+
+const singleShortestPath = function() {
+  const q = `
+    MATCH (martin :Person { name: 'Martin Sheen' }), (oliver :Person { name: 'Oliver Stone' }),
+      p = shortestPath((martin)-[*0..15]-(oliver))
+    RETURN p
+  `
+  console.log(chalk.dim.magenta('[Q]'), chalk.dim(q))
+  return GraphUtils.runQueryAndFetchResult(q)
+  .then(result => {
+    console.log(chalk.dim.magenta('[Result]'), chalk.dim(JSON.stringify(result)))
+    _.forEach(result[0].p.segments, segment => {
+      let pathString = ''
+      pathString += `(${segment.start.properties.name} :${segment.start.labels[0]})`
+      pathString += `-[:${segment.relationship.type}]-`
+      pathString += `(${segment.end.properties.name} :${segment.end.labels[0]})`
+      console.log(chalk.green(pathString))
+    })
+    return Promise.resolve()
+  })
+}
+
 const main = () => {
   console.log(chalk.bold.cyan('[STARTING]'))
   return DeleteGraph.deleteAllFromGraph()
@@ -75,6 +180,24 @@ const main = () => {
   })
   .then(() => {
     return multipleRelationshipTypes()
+  })
+  .then(() => {
+    return addSpaceRelationship()
+  })
+  .then(() => {
+    return multipleRelationships()
+  })
+  .then(() => {
+    return variableLengthRelationship()
+  })
+  .then(() => {
+    return coActors()
+  })
+  .then(() => {
+    return namedPath()
+  })
+  .then(() => {
+    return singleShortestPath()
   })
   .then(() => {
     console.log(chalk.bold.cyan('[ENDING]'))
